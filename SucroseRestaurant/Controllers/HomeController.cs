3b560c5afeca50c46822a9fd349a28cf8ee08935
Trương An Khang <ankhang897979@gmail.com>
2023-04-03 13:29:03 +0700

    Add food model

--- SucroseRestaurant/Controllers/HomeController.cs
+++ SucroseRestaurant/Controllers/HomeController.cs
@@ -6,11 +6,9 @@ namespace SucroseRestaurant.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
         }
 
         public IActionResult Index()
@@ -18,15 +16,5 @@ namespace SucroseRestaurant.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
---
+++ SucroseRestaurant/Models/Foods/Food.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SucroseRestaurant.Models.Foods
+{
+    public class Food
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please insert the food name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please insert the food decription")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Please insert the food price")]
+        public float Price { get; set; }
+
+        [Required(ErrorMessage = "Please insert the food Category")]
+        public string Category { get; set; }
+
+        public string PhotoPath { get; set; }
+
+    }
+}
